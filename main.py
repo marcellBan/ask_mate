ee7f96d16dc6709e7a6e35e211321397fafae7d1
Bán Marcell <afga93+cc@gmail.com>
2017-05-02 13:25:09 +0200

    main tweaks

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-'''AskMate Q&A website
+'''
+AskMate Q&A website
 by SzószKód
 '''
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -10,24 +11,24 @@ def index():
     return 'Index, all questions with default sorting.'
 
 
-@app.route('/sort-by=<sort_type>')
-def sorted_index(sort_type):
-    return 'Index, sorted by %s.' % sort_type
+@app.route('/sort')
+def sorted_index():
+    return 'Index, sorted by time={} & title={}.'.format(request.args.get('time', request.args.get('title')))
 
 
-@app.route('/question/new')
+@app.route('/question/new', methods=['GET', 'POST'])
 def new_question():
     return 'New question.'
 
 
 @app.route('/question/<int:q_id>')
 def display_question(q_id=0):
-    return 'Display question with id %s.' % q_id
+    return 'Display question with id {}.'.format(q_id)
 
 
-@app.route('/question/<int:q_id>/new-answer')
+@app.route('/question/<int:q_id>/new-answer', methods=['GET', 'POST'])
 def new_answer(q_id=0):
-    return 'New answer to question with id %s.' % q_id
+    return 'New answer to question with id {}.'.format(q_id)
 
 
 @app.errorhandler(404)
@@ -36,4 +37,4 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.run(debug=True)

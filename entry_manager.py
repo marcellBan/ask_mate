6d7a2c4c5c8333d0a6be53549a194d764beea648
martonMeszaros <marton.m95@gmail.com>
2017-05-02 23:39:36 +0200

    Updated entry_manager.py to handle incorrect question form

--- entry_manager.py
+++ entry_manager.py
@@ -3,20 +3,24 @@ entry manager module for AskMate
 by SzószKód
 '''
 
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, flash
 from data_manager import load_data, save_data
 import time
 
 
 def add_question():
     if request.method == 'GET':
-        return render_template("new_question.html")
+        return render_template('new_question.html')
     elif request.method == 'POST':
         if len(request.form.get('message')) < 10:
-            return render_template("new_question.html")  # TODO: pass in the previous data
+            flash('Your question isn\'t long enough!')
+            return render_template(
+                'new_question.html',
+                form_title=request.form['title'], form_message=request.form['message']
+            )
         else:
             data = load_data()
-            maxid = max(data.keys())
+            maxid = max(data.keys())  # TODO Throws an error for empty sequence.
             data[maxid + 1] = {
                 'id': maxid + 1,
                 'submission_time': int(time.time()),

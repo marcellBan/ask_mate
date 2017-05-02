06371fa9576058d1715af4f5d610beede0b3a895
Bán Marcell <afga93+cc@gmail.com>
2017-05-02 20:23:49 +0200

    got new question working

--- entry_manager.py
+++ entry_manager.py
@@ -3,8 +3,28 @@ entry manager module for AskMate
 by SzószKód
 '''
 
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, url_for
+from data_manager import load_data, save_data
+import time
 
 
 def add_question():
-    return render_template("new_question.html")
+    if request.method == 'GET':
+        return render_template("new_question.html")
+    elif request.method == 'POST':
+        if len(request.form.get('message')) < 10:
+            return render_template("new_question.html")  # TODO: pass in the previous data
+        else:
+            data = load_data()
+            maxid = max(data.keys())
+            data[maxid + 1] = {
+                'id': maxid + 1,
+                'submission_time': int(time.time()),
+                'view_number': 0,
+                'vote_number': 0,
+                'title': request.form.get('title'),
+                'message': request.form.get('message'),
+                'image': request.form.get('image')
+            }
+            save_data(data)
+            return redirect(url_for('display_question', q_id=maxid + 1))

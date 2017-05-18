e3c9e84b2bb691b2148ea79e72a581609f10d2c3
Bán Marcell <afga93+cc@gmail.com>
2017-05-18 15:36:47 +0200

    added newline fixes

--- display.py
+++ display.py
@@ -10,6 +10,8 @@ from flask import render_template, redirect, request, session, url_for
 def display_questions():
     questions_list = data_manager.get_questions()
     for question in questions_list:
+        question['message'] = replace_line_breaks(question['message'])
+        question['title'] = replace_line_breaks(question['title'])
         question['answer_count'] = len(data_manager.get_answers(question['id']))
     return render_template('list.html', question_list=questions_list, page='')
 
@@ -18,17 +20,26 @@ def display_one_question(question_id):
     question = data_manager.get_question(question_id)
     question['view_number'] += 1
     data_manager.update_question(question)
+    question['message'] = replace_line_breaks(question['message'])
+    question['title'] = replace_line_breaks(question['title'])
     answers = data_manager.get_answers(question_id)
     for answer in answers:
+        answer['message'] = replace_line_breaks(answer['message'])
         answer['comments'] = data_manager.get_comments_for_answer(answer['id'])
+        for comment in answer['comments']:
+            comment['message'] = replace_line_breaks(comment['message'])
     question['answer_count'] = len(answers)
     question['comments'] = data_manager.get_comments_for_question(question_id)
+    for comment in question['comments']:
+        comment['message'] = replace_line_breaks(comment['message'])
     return render_template('question.html', question=question, answers=answers)
 
 
 def display_five_latest_questions():
     questions_list = data_manager.get_questions(limit=5)
     for question in questions_list:
+        question['message'] = replace_line_breaks(question['message'])
+        question['title'] = replace_line_breaks(question['title'])
         question['answer_count'] = len(data_manager.get_answers(question['id']))
     return render_template('list.html', question_list=questions_list, page='index')
 
@@ -54,6 +65,8 @@ def display_sorted_questions():
                 session['sorting'].append((aspect, request.args.get(aspect)))
     questions = data_manager.get_questions(sorting=session['sorting'])
     for question in questions:
+        question['message'] = replace_line_breaks(question['message'])
+        question['title'] = replace_line_breaks(question['title'])
         question['answer_count'] = len(data_manager.get_answers(question['id']))
     return render_template('list.html', question_list=questions, page='sorted')
 
@@ -62,3 +75,7 @@ def clear_sorting():
     if session.get('sorting') is not None:
         del(session['sorting'])
     return redirect(url_for('list_index'))
+
+
+def replace_line_breaks(string):
+    return string.replace('\n', '<br>')

9960314847941b21a947884b518bd3ab922c863a
Miklós Torbics <torbics.miklos@gmail.com>
2017-06-01 16:13:53 +0200

    Handle bad requests (Error 405)

--- main.py
+++ main.py
@@ -191,6 +191,11 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(405)
+def page_not_found(error):
+    return render_template('405.html'), 405
+
+
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500.html', error_message=error), 500

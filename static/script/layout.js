7a80352075ca7502aa95f17e86f680c80f7feaed
martonMeszaros <marton.m95@gmail.com>
2017-06-03 23:24:25 +0200

    Updated style for mobile header

--- static/script/layout.js
+++ static/script/layout.js
@@ -1,14 +1,12 @@
 var pageMenu = document.getElementById('page-menu');
 var pageMenuOpenButton = document.getElementById('page-menu-open-button');
 var pageMenuCloseButton = document.getElementById('page-menu-close-button');
+var sideNavShadow = document.getElementById('side-nav-shadow');
 
-function closePageMenu() {
-    pageMenu.classList.remove('visible');
+function togglePageMenu() {
+    pageMenu.classList.toggle('visible');
+    sideNavShadow.classList.toggle('visible');
 }
 
-function openPageMenu() {
-    pageMenu.classList.add('visible');
-}
-
-pageMenuOpenButton.addEventListener('click', openPageMenu, false);
-pageMenuCloseButton.addEventListener('click', closePageMenu, false);
+pageMenuOpenButton.addEventListener('click', togglePageMenu, false);
+pageMenuCloseButton.addEventListener('click', togglePageMenu, false);
